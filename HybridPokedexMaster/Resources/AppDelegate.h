1d5d17c155c70794e37ac16ae8ba7e4b417a6a70
Jason Mandozzi <jason.mandozzi@gmail.com>
2019-07-02 09:57:46 -0600

    file organization

---
+++ HybridPokedexMaster/Resources/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HybridPokedexMaster
+//
+//  Created by Jason Mandozzi on 7/2/19.
+//  Copyright © 2019 Jason Mandozzi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
